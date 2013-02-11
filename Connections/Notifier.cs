349cf52d51fb45f3bd870bdf2e6d4e21c8eda92c
Tomas Ekeli <tomas.ekeli@gmail.com>
2013-02-11 22:22:48 +0100

    Notifier now has groups

---
+++ Connections/Notifier.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNet.SignalR;
+
+namespace LiveDashboard.Web.Connections
+{
+    public class Notifier
+    {
+        readonly List<Group> groups = new List<Group>();
+
+        public void AddGroup(Group toAdd)
+        {
+            if (groups.All(g => g.Name != toAdd.Name))
+            {
+                groups.Add(toAdd);
+            }
+        }
+
+        public void Notify()
+        {
+            var context = GlobalHost.ConnectionManager.GetConnectionContext<RandomValue>();
+
+            foreach (var group in groups)
+            {
+                var number = new Random().Next(group.Min, group.Max);
+                context.Groups.Send(group.Name, number);
+            }
+        }
+    }
+
+    public class Group
+    {
+        public string Name { get; set; }
+        public int Min { get; set; }
+        public int Max { get; set; }
+    }
+}
--- Connections/RandomValue.cs
+++ Connections/RandomValue.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNet.SignalR;
@@ -7,30 +7,63 @@ namespace LiveDashboard.Web.Connections
 {
     public class RandomValue : PersistentConnection
     {
-        static Thread thread;
+        static Thread backgroundThread;
         static int connections;
+        static bool keepGoing;
+        Notifier notifier;
 
         protected override Task OnConnected(IRequest request, string connectionId)
         {
-            if (thread == null)
+            keepGoing = true;
+            notifier = new Notifier();
+
+            if (backgroundThread == null)
             {
-                thread = new Thread(() =>
+                backgroundThread = new Thread(() =>
                 {
-                    while (true)
+                    while (keepGoing)
                     {
-                        var notifier = new Notifier();
                         notifier.Notify();
                         Thread.Sleep(1000);
                     }
 
                 });
 
-                thread.Start();
+                backgroundThread.Start();
             }
 
-            connections = 1;
+            var groupName = GetStringValue(request, "group") ?? "default";
+            var minValue = GetValue(request, "min") ?? 0;
+            var maxValue = GetValue(request, "max") ?? 100;
+
+            Groups.Add(connectionId, groupName);
+            notifier.AddGroup(new Group
+            {
+                Name = groupName,
+                Min = minValue,
+                Max = maxValue
+            });
+
+            connections += 1;
 
             return base.OnConnected(request, connectionId);
+        }
+
+        static string GetStringValue(IRequest request, string keyName)
+        {
+            return request.QueryString.AllKeys.Contains(keyName)
+                       ? request.QueryString[keyName]
+                       : null;
+        }
+
+        static int? GetValue(IRequest request, string keyName)
+        {
+            int minValue;
+            if (request.QueryString.AllKeys.Contains(keyName) && int.TryParse(request.QueryString[keyName], out minValue))
+            {
+                return minValue;
+            }
+            return null;
         }
 
         protected override Task OnDisconnected(IRequest request, string connectionId)
@@ -38,22 +71,10 @@ namespace LiveDashboard.Web.Connections
             connections--;
             if (connections < 1)
             {
-                thread.Abort();
-                thread = null;
+                keepGoing = false;
+                backgroundThread = null;
             }
             return base.OnDisconnected(request, connectionId);
-        }
-    }
-
-
-    public class Notifier
-    {
-        public void Notify()
-        {
-            var number = new Random().Next(0, 100);
-
-            var context = GlobalHost.ConnectionManager.GetConnectionContext<RandomValue>();
-            context.Connection.Broadcast(number);
         }
     }
 }
